dde4500cb7f96857e1445c92301e43462f1d2892
Maahisinghgit <maahisingh07m@gmail.com>
2022-06-15 19:39:41 +0530

    44

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'InputPage.dart';
 
 void main() {
   runApp(BMICalculator());
@@ -7,33 +8,12 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-     theme: ThemeData(
-       primaryColor: Color(0xFF0A0E21),
-       accentColor: Colors.purple,
-     ),
+   theme: ThemeData.dark().copyWith(
+     primaryColor: Color(0xFF0A0E21),
+     scaffoldBackgroundColor: Color(0xFF0A0E21)
+   ),
       home: InputPage(),
     );
   }
 }
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
 
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text('Body Mass Index')),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {  },
-        child: Icon(Icons.add),
-      ),
-    );
-  }
-}
