f9bab708591b5ec891439c7fe3a89c153de79e4a
Maahisinghgit <maahisingh07m@gmail.com>
2022-06-16 20:43:10 +0530

    onpress button functionlity

--- lib/InputPage.dart
+++ lib/InputPage.dart
@@ -136,11 +136,11 @@ class _InputPageState extends State<InputPage> {
                         Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
-                           RoundIconButton(),
+                           RoundIconButton(icon: FontAwesomeIcons.plus,),
                             SizedBox(
                               width: 10.0,
                             ),
-                           RoundIconButton(),
+                           RoundIconButton(icon: FontAwesomeIcons.minus),
                           ],
                         ),
                       ],
@@ -167,17 +167,23 @@ class _InputPageState extends State<InputPage> {
   }
 }
 class RoundIconButton extends StatelessWidget {
-  const RoundIconButton({Key? key}) : super(key: key);
 
+  RoundIconButton ({required this.icon,required this.onPressed});
+  final IconData icon;
+  final Function onPressed;
   @override
   Widget build(BuildContext context) {
     return RawMaterialButton(
+      onPressed: (){},
+      elevation: 6.0,
+      child: Icon(icon),
+      constraints: BoxConstraints.tightFor(
+          width: 56.0,
+          height: 56.0
+      ),
       shape: CircleBorder(),
       fillColor: Color(0xFF4C4F5E),
-      constraints: BoxConstraints.tightFor(
-        width: 56.0,
-        height: 56.0
-      ),
+
     );
   }
 }
